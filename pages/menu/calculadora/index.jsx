0f8875874aac033747f6a794087e40cfef3dc30f
Messias6 <messias6@gmail.com>
2023-02-05 22:26:38 +0000

    Update index.jsx

--- pages/menu/calculadora/index.jsx
+++ pages/menu/calculadora/index.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import styles from "../../../styles/Home.module.css";
 
 export default function Calculadora() {
@@ -12,11 +12,11 @@ export default function Calculadora() {
   const [dose, setDose] = useState(0);
 
   const handleChange = (e) => {
-    if (e.target.value === 'Nora') {
+    if (e.target.value === "Nora") {
       setMedicationVolume(16);
       setSolutionVolume(250);
       setMedicationConcentration(1);
-    } else if (e.target.value === 'Dobuta') {
+    } else if (e.target.value === "Dobuta") {
       setMedicationVolume(80);
       setSolutionVolume(250);
       setMedicationConcentration(12.5);
@@ -24,10 +24,14 @@ export default function Calculadora() {
   };
 
   const calculateDose = () => {
-    setDose((((
-      infusionRate * (medicationConcentration * medicationVolume)) / solutionVolume) / 60) / patientWeight * 1000);
+    setDose(
+      ((infusionRate * (medicationConcentration * medicationVolume)) /
+        solutionVolume /
+        60 /
+        patientWeight) *
+        1000
+    );
   };
-
 
   return (
     <div>
@@ -35,9 +39,9 @@ export default function Calculadora() {
       <h2 className={styles.subtitulo}>Calculadora de Doses</h2>
 
       <nav className={styles.navegacao}>
-
-        <Link className={styles.navegacaolink} href="/">MenuPrincipal</Link>
-
+        <Link className={styles.navegacaolink} href="/">
+          MenuPrincipal
+        </Link>
       </nav>
 
       <div className={styles.container}>
@@ -46,8 +50,9 @@ export default function Calculadora() {
           <option value="Nora">Nora 4:1</option>
           <option value="Dobuta">Dobuta 4:1</option>
         </select>
-        <label > Peso do paciente (kg):</label>
-        <input className={styles.input}
+        <label> Peso do paciente (kg):</label>
+        <input
+          className={styles.input}
           type="number"
           min={40}
           max={150}
@@ -56,7 +61,8 @@ export default function Calculadora() {
         />
 
         <label>Velocidade de infusão:</label>
-        <input className={styles.input}
+        <input
+          className={styles.input}
           type="number"
           min={0}
           max={200}
@@ -65,21 +71,24 @@ export default function Calculadora() {
         />
 
         <label>Volume da medicação:</label>
-        <input className={styles.input}
+        <input
+          className={styles.input}
           type="number"
           value={medicationVolume}
           onChange={(e) => setMedicationVolume(e.target.value)}
         />
 
         <label>Volume da solução:</label>
-        <input className={styles.input}
+        <input
+          className={styles.input}
           type="number"
           value={solutionVolume}
           onChange={(e) => setSolutionVolume(e.target.value)}
         />
 
         <label>Concentração da ampola da medicação:</label>
-        <input className={styles.input}
+        <input
+          className={styles.input}
           type="number"
           value={medicationConcentration}
           onChange={(e) => setMedicationConcentration(e.target.value)}
@@ -87,14 +96,20 @@ export default function Calculadora() {
 
         <button onClick={calculateDose}>Calcular dose</button>
 
-        <label className={styles.resultado}>Dose: {parseFloat(dose).toFixed(2)} mcg/kg/min</label>
+        <label className={styles.resultado}>
+          Dose: {parseFloat(dose).toFixed(2)} mcg/kg/min
+        </label>
 
         <div className={styles.linkexterno}>
-          <a target="_blank" href="https://www.mediafire.com/file/nls50ko0bqdtsvq/Planilha_de_dilui%25C3%25A7%25C3%25A3o.xlsx/file">Planilha com diversas drogas</a>
+          <a
+            target="_blank"
+            href="https://www.mediafire.com/file/nls50ko0bqdtsvq/Planilha_de_dilui%25C3%25A7%25C3%25A3o.xlsx/file"
+            rel="noreferrer noopener"
+          >
+            Planilha com diversas drogas
+          </a>
         </div>
       </div>
-
-
-    </div >
+    </div>
   );
 }
